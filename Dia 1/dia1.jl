5492291a813e71025870a4f00a853940e3c136b3
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-08 12:16:37 -0300

    Variaveis A e B

--- Dia 1/dia1.jl
+++ Dia 1/dia1.jl
@@ -5,11 +5,13 @@ println(100)
 
 println("Ola Mundo!! ", 10, ", o numero é ", 10)
 
-printstyled("Ola Mundo!!!\n"; color = :red)
+printstyled("Ola Mundo!!!\n"; color=:red)
 
-printstyled("Ola Mundo!!!!\n"; color = :blue)
+printstyled("Ola Mundo!!!!\n"; color=:blue)
 
-printstyled("Ola Mundo!!!!!\n"; color = :yellow)
+printstyled("Ola Mundo!!!!!\n"; color=:yellow)
+
+
 
 # Operações matematicas basicas
 
@@ -19,11 +21,32 @@ println("Subtração: 3 - 2 = ", 3 - 2)
 
 println("Multiplicação: 2 * 3 = ", 2 * 3)
 
+println("Potenciação: 2 elevado ao cubo = 2 ^ 3 = 2 * 2 * 2 ", 2^3)
+
 println("Divisão: 4 / 2 = ", 4 / 2)
 
 println("O Resto da Divisão 5 / 2: 5 % 2 = ", 5 % 2)
 
+
+
 # Atribuição de valores e texto em Variaveis
+a = 20
+b = 3
+println("Valor de a é: ", a)
+println("$a + $a = ", a + 2)
+println("$a - 1 = ", a - 1)
+println("$a * 2 = ", a * 2)
+println("$a ^ 2 = ", a ^ 2)
+println("$a / 2 = ", a / 2)
+println("$a % 3 = ", a % 2)
+
+println("Valor de a e b é: $a e $b")
+println("$a + $b = ", a + b)
+println("$a - $b = ", a - b)
+println("$a * $b = ", a * b)
+println("$a ^ $b = ", a^b)
+println("$a / $b = ", a / b)
+println("$a % $b = ", a % b)
 
 # Aplicação de operadores matematicas em Variaveis
 

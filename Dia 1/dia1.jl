449988da50f93d72e069d94dec8a22e40a551eed
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-08 12:04:15 -0300

    Adicionando exibição basica de textos e numeros

---
+++ Dia 1/dia1.jl
@@ -0,0 +1,24 @@
+# Exibir valores e textos
+println("Ola Mundo!")
+
+println(100)
+
+println("Ola Mundo!! ", 10, ", o numero é ", 10)
+
+printstyled("Ola Mundo!!!\n"; color = :red)
+
+printstyled("Ola Mundo!!!!\n"; color = :blue)
+
+printstyled("Ola Mundo!!!!!\n"; color = :yellow)
+
+# Operações matematicas basicas
+
+# Atribuição de valores e texto em Variaveis
+
+# Aplicação de operadores matematicas em Variaveis
+
+# Definindo funções 1
+
+# Definindo funções 2
+
+

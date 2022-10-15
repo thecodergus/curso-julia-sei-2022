9cf55af883e9093d7fb83713b50a35a1951d7eaf
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-14 21:36:37 -0300

    Add jacobiana

--- Dia 2/dia2.jl
+++ Dia 2/dia2.jl
@@ -13,11 +13,19 @@ using Symbolics
 
 # Derivadas 
 @variables t u # Declarando as varaiveis que faram parte das equações
-D = Differential(t) # Definindo qual a variavel que será derivada
 z = t^2 + 5t + 10u  # Definindo equação
-dz = D(z) # Derivando a equação defnida
+dz = Symbolics.derivative(z, t) # Derivando a equação defnida
 println("f(t, u) = $(z)")
 println("df(t, u) = $(expand_derivatives(dz))")
+
+## Derivada de segunda e terceira ordem
+D2 = Differential(t)^2
+D3 = Differential(t)^3
+dz2 = D2(z)
+dz3 = D3(z)
+println("f(t, u)'' = $(expand_derivatives(dz2))")
+println("f(t, u)''' = $(expand_derivatives(dz3))")
+
 
 ## Usando funções simbolicas e Derivadas destas como funções
 h = @eval $(build_function(expand_derivatives(z), [t, u]))

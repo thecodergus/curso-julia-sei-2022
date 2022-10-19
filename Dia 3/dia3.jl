5e3ba45f463e9e2b8b535bb6fb81f187aee68576
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-10-19 17:56:01 -0300

    Mais coisas

--- Dia 3/dia3.jl
+++ Dia 3/dia3.jl
@@ -2,4 +2,11 @@ using CSV, DataFrames
 
 df = CSV.File("imdb_data.csv") |> DataFrame
 
-sort(first(df, 5), ["movie_year", "genre/0"])
+# sort(first(df, 5), ["movie_year", "genre/0"])
+
+# first(sort(df, ["movie_name"], rev=false), 3)
+# show(df, allrows=true)
+# df2 = select(df, [:rank, :movie_name, :movie_year])
+
+# people = DataFrame(ID = [1, 2], Name = ["Morgan Freeman", "Al Pacino"])
+df[df.movie_year .>= 2020, :]
